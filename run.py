5494ad135760e7785b91bd8757a480f62b29382b
lupt <lupt@luptdeMacBook-Pro.local>
2017-08-27 16:21:32 +0800

    fix bug

--- run.py
+++ run.py
@@ -7,24 +7,22 @@ from proxyCrawler import ProxyCrawler
 
 
 def main():
-    logging.info('server start running...')
-    if IS_SERVER and ACTIVATE_CRAWL_PROXY:    
-        proxy_crawler = ProxyCrawler()
-        proxy_checker = ProxyChecker()
+    logging.info('server start running...')  
 
-        proxy_crawler_process = Process(target=proxy_crawler.run)
-        proxy_checker_process = Process(target=proxy_checker.run)
-
-        proxy_crawler_process.start()
-        proxy_checker_process.start()
-        
-        proxy_crawler_process.join()
-        proxy_checker_process.join()
-
-    
+    proxy_crawler = ProxyCrawler()
+    proxy_checker = ProxyChecker()
     group_crawler = GroupCrawler()
+
+    proxy_crawler_process = Process(target=proxy_crawler.run)
+    proxy_checker_process = Process(target=proxy_checker.run)
     group_crawler_process = Process(target=group_crawler.run)
+
+    proxy_crawler_process.start()
+    proxy_checker_process.start()
     group_crawler_process.start()
+    
+    proxy_crawler_process.join()
+    proxy_checker_process.join()
     group_crawler_process.join()
 
 if __name__ == '__main__':
